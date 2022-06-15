daa4033b2f6561ef9073f12894e8639f4ce3706b
abeel50 <abeelmuhammad50@gmail.com>
2022-06-15 17:56:09 +0500

    77%

---
+++ MaxCounters.py
@@ -0,0 +1,12 @@
+def solution(N, A):
+    # write your code in Python 3.6
+    sol = [0] * N
+    m = 0
+    for i in A:
+        if i > 0 and i <= N:
+            sol[i-1] += 1
+            if sol[i-1] > m:
+                m = sol[i-1]
+        elif i == N+1:
+            sol = [m] * N
+    return sol
